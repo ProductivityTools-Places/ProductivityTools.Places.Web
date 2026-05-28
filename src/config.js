ba35b8cdbd7e588c567e101c1f7530150802a053
Pawel Wujczyk <pwujczyk@gmail.com>
2026-05-28 21:22:09 +0200

    prod test

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const dev = {
     PATH_BASE: 'http://127.0.0.1:8080',
-    GOOGLE_API_KEY: 'AIzaSyALTp_8O1-z9UV-5hvXEjCdzpEWkTQZJJY'
+    GOOGLE_API_KEY: 'AIzaSyAi2YG0yEOJ00sLKM63Vbxfpdk_SqL7UvI'
 }
-
+//this should be taken from firebase
 const prd = {
     PATH_BASE: 'https://places-api-dot-ptprojectsweb.ew.r.appspot.com',
-    GOOGLE_API_KEY: 'AIzaSyALTp_8O1-z9UV-5hvXEjCdzpEWkTQZJJY'
+    GOOGLE_API_KEY: 'AIzaSyBxY4oT4SZd5r-nZiM1eFFnUCcC3UxgYr4'
 }
 
 //export const config=prd;
